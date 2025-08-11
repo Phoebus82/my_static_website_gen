05d1e2bdecb21165f8a0b090b7323a8fb08f3baf
Phoebus82 <foivosvoulgaris82@gmail.com>
2025-08-12 01:12:00 +0200

    almost ready?

--- src/file_based_functions.py
+++ src/file_based_functions.py
@@ -18,7 +18,7 @@ def copy_from_a_to_b(a, b):
             os.mkdir(i_b_path)
             copy_from_a_to_b(i_a_path, i_b_path)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(f"Generating page from {from_path}, to {dest_path}, using {template_path}")
     with open(from_path,"r") as fi:
         md = fi.read()
@@ -27,20 +27,21 @@ def generate_page(from_path, template_path, dest_path):
     with open(template_path, "r") as temp:
         tempo = temp.read()
     tempor = tempo.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    tempor = tempor.replace('href="/', f'href="{base_path}').replace('src="/', f'src="{base_path}')
     if os.path.dirname(dest_path) != "":
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as fo:
         fo.write(tempor)
     
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, base_path):
     for entry in os.listdir(dir_path_content):
         entry_path = os.path.join(dir_path_content, entry)
         if os.path.isfile(entry_path) and entry_path.endswith(".md"):
             dest_file = entry.replace(".md", ".html")
-            generate_page(entry_path, template_path, os.path.join(dest_dir_path, dest_file))
+            generate_page(entry_path, template_path, os.path.join(dest_dir_path, dest_file), base_path)
         else:
             new_dest = os.path.join(dest_dir_path, entry)
             os.makedirs(new_dest, exist_ok=True)
-            generate_pages_recursive(entry_path, template_path, new_dest)
+            generate_pages_recursive(entry_path, template_path, new_dest, base_path)
 
     
--- src/main.py
+++ src/main.py
@@ -1,12 +1,19 @@
 print("hello world")
 from textnode import *
 from file_based_functions import *
+import sys
+
+if len(sys.argv) > 1:
+    base_path = sys.argv[1]
+else:
+    base_path = "/"
+
 def main():
    #ob = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     #print(ob)
-    clean_b("public")
-    copy_from_a_to_b("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    clean_b("docs")
+    copy_from_a_to_b("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", base_path)
 
-    
+
 main()
